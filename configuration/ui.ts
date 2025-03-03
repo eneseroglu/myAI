34224a6275d02cebd44ec84451a7acb19ac3e1f3
Enes Eroglu <89422667+eneseroglu@users.noreply.github.com>
2025-03-03 14:25:54 -0500

    Update ui.ts
    updated the page title

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `Medical Specialist 2025`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Enes @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Entire Chat`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `Enes's AI Assistant`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
