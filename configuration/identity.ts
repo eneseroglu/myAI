c7f50e3ab6e1ffccdb64a7652d1adfc9604e7a46
Enes Eroglu <89422667+eneseroglu@users.noreply.github.com>
2025-03-03 14:23:19 -0500

    Update identity.ts
    gave a role and tone

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Enes Eroglu`;
 export const OWNER_DESCRIPTION: string = `Hi! My name is Enes Eroglu and I am currently a senior at UNC Chapel Hill, double majoring in CS and Neuroscience. My post-grad plans entail going to UNC medical school, which has inspired me to create this medical specialist AI. Some of my hobbies include playing various sports (tennis, pickleball, basketball) and reading. I hope you enjoy utilizing my AI resources!`;
 export const AI_NAME: string = `EE`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_TONE: string = `Friendly and helpful`;
+export const AI_ROLE: string = `Virtual assistant providing answers and guidance, specialized in medical information`;
