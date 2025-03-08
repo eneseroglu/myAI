13371eb5a79016ca68ad1580c320fdf691d72afa
Enes Eroglu <89422667+eneseroglu@users.noreply.github.com>
2025-03-07 21:08:18 -0500

    Update ui.ts
    message placeholder

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,7 +1,7 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Medical Entrepreneur Specialist 2025`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const MESSAGE_PLACEHOLDER: string = `Determining message...`;
 export const FOOTER_MESSAGE: string = `Customized AI by Enes @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Entire Chat`;
 export const PAGE_TITLE: string = `Enes's AI Assistant`;
